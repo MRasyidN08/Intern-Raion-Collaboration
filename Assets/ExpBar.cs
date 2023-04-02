2c2b738b8660dcb479383e5e20afc9c915584c45
BagasBikang <bagasadit78@gmail.com>
2023-04-02 12:38:30 +0700

    fix level

--- Assets/ExpBar.cs
+++ Assets/ExpBar.cs
@@ -1,23 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class GainExp : MonoBehaviour
-{
+// public class GainExp : MonoBehaviour
+// {
 
-    public PlayerStat player;
-    public Image FillBar;
-    public Text LevelText;
+//     public PlayerStat player;
+//     public Image FillBar;
+//     public Text LevelText;
 
-    private float expAmount;
+//     private float expAmount;
 
-    // Update is called once per frame
-    void Update()
-    {
-        expAmount = player.Exp;
-        expFillimage.fillAmount = exp/player.maxExp;
-        print(player.Level);
-        levelText.text = player.Level.ToString();
-    }
-}
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         expAmount = player.Exp;
+//         expFillimage.fillAmount = exp/player.maxExp;
+//         print(player.Level);
+//         levelText.text = player.Level.ToString();
+//     }
+// }
--- Assets/Scripts/LifeCount.cs
+++ Assets/Scripts/LifeCount.cs
@@ -1,32 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class LifeCount : MonoBehaviour
-{
-   public Image[] lives;
-   public int livesRemaining;
+// public class LifeCount : MonoBehaviour
+// {
+//    public Image[] lives;
+//    public int livesRemaining;
 
-   public void LoseLife()
-   {
-        //if livesRemaining 0 do nothing
-        if(livesRemaining==0)
-            return;
-        //decrease the value life
-        livesRemaining--;
-        //hiding one image of life count
-        lives[livesRemaining].enabled = false;
+//    public void LoseLife()
+//    {
+//         //if livesRemaining 0 do nothing
+//         if(livesRemaining==0)
+//             return;
+//         //decrease the value life
+//         livesRemaining--;
+//         //hiding one image of life count
+//         lives[livesRemaining].enabled = false;
 
-        //if we run out the lives
-        if(livesRemaining == 0){
-            Debug.Log("You Lost");
-        }
-   }
+//         //if we run out the lives
+//         if(livesRemaining == 0){
+//             Debug.Log("You Lost");
+//         }
+//    }
 
-   private void Update()
-   {
-        if(Input.GetKeyDown(KeyCode.Return))
-            LoseLife();
-   }
-}
+//    private void Update()
+//    {
+//         if(Input.GetKeyDown(KeyCode.Return))
+//             LoseLife();
+//    }
+// }
