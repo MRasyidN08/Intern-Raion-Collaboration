05b1a7f438e400cdd345c3bdb5b4ff3ad6a7abf0
MRasyidN08 <128342484+MRasyidN08@users.noreply.github.com>
2023-04-08 14:57:47 +0700

    Animasi mati dan kena dmg

--- Assets/Scripts/LifeCount.cs
+++ Assets/Scripts/LifeCount.cs
@@ -5,31 +5,48 @@ using UnityEngine.UI;
 
 public class LifeCount : MonoBehaviour
 {
-   public Image[] lives;
-   public int livesRemaining;
-   public PlayerStat player;
+    public Image[] lives;
+    public int livesRemaining;
+    public PlayerStat player;
+    private Animator animasi;
+    private bool Dead;
 
-   public void LoseLife()
-   {
-        //if livesRemaining 0 do nothing
-        if(livesRemaining==0)
-            return;
+    private void Awake()
+    {
+        animasi = GetComponent<Animator>();
+    }
+
+    public void LoseLife()
+    {
         //decrease the value life
         livesRemaining--;
         //hiding one image of life count
         lives[livesRemaining].enabled = false;
         //if we run out the lives
-        if(livesRemaining == 0){
-            Debug.Log("You Lost");
+        if(livesRemaining > 0){
+            animasi.SetTrigger("hurt");
+        } else{
+            if(!Dead){
+            animasi.SetTrigger("die");
+            GetComponent<PlayerMovement>().enabled = false;
+            Dead = true;
+            }
         }
-   }
+    }
 
-   void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Enemy")
-        {
-            LoseLife();
+    // void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (other.tag == "Enemy")
+    //     {
+    //         LoseLife();
             
+    //     }
+    // }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)){
+            LoseLife();
         }
     }
 
